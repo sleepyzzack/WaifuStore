6b181baed97eea701a2dd72855fef3b2ef8243a5
Bryan Lazo <lebry4n@gmail.com>
2022-09-30 14:51:42 -0600

    Reparando consultas modificar

--- controladores/modificar_producto.php
+++ controladores/modificar_producto.php
@@ -1,21 +1,19 @@
 <?php
 
-if [!empty($_POST["btnregistrar"])]{
-    if (!empty($_POST["titulo"]) and empty($_POST["categoria"]) and empty($_POST["precio"] and empty($_POST["descripcion"]))){
-        $id=$_POST["id"];
-        $nombre=$_POST["titulo"];
-        $categoria=$_POST["categoria"];
-        $precio=$_POST["precio"];
-        $descripcion=$_POST["descripcion"];
-        $sql=$conexion->query("update producto set nombre='$nombre', cantegoria='$categoria', precio='$precio', descripcion='$descripcion' where el id_product='$id'")
-        if ($sql==){
+if (!empty($_POST["btnregistrar"])) {
+    if (!empty($_POST["titulo"]) and empty($_POST["categoria"]) and empty($_POST["precio"] and empty($_POST["descripcion"]))) {
+        $id = $_POST["id"];
+        $nombre = $_POST["titulo"];
+        $categoria = $_POST["categoria"];
+        $precio = $_POST["precio"];
+        $descripcion = $_POST["descripcion"];
+        $sql = $conexion->query("UPDATE books SET book_name='$nombre', book_category='$categoria', book_price='$precio', book_description='$descripcion' WHERE id_books='$id'");
+        if ($sql == 1) {
             header("location:index.php");
-        }else{
+        } else {
             echo "<div class='alert alert-danger'>Error al Modificar</div>";
         }
-    }else{
+    } else {
         echo "<div class='alert alert-warning'>Campos Vacios</div>";
     }
 }
-
-?>
