3931623963789db4362bb8c0882b213a86e4e25c
sleepyzzack <109996822+sleepyzzack@users.noreply.github.com>
2022-10-02 11:33:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="./vistas/assets/css/block/textarea.css">
     <script src="https://kit.fontawesome.com/06e9d41eed.js" crossorigin="anonymous"></script>
+    <script src="extras\pop.js" crossorigin="juanito"></script>
 </head>
 
 <body>
