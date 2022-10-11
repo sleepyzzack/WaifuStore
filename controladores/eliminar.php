759f92b410c471d5bc727bdb78a8d603bc7f1aec
sleepyzzack <tylinux1@gmail.com>
2022-10-11 15:38:42 -0600

    correccion a archivo eliminiar.php

--- controladores/eliminar.php
+++ controladores/eliminar.php
@@ -6,7 +6,7 @@ if(!empty($_GET["id"])){
     $sql = $connect->query(" DELETE FROM books WHERE id_books=$id ");
 
     if($sql==1){
-        header('Location: index.php');
+        header('Location: adminIndex.php');
     }else{
         echo '<div class="alert alert-danger" id="borrame" onClick="borrar()">no se pudo eliminar F OwO</div>';
     }
